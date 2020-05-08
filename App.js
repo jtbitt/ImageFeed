618cb553b00c7002a126380a8652e11d6e5d8f51
Jay Bittner <jtbitt@gmail.com>
2020-05-07 20:10:50 -0500

    Setting up modal

--- App.js
+++ App.js
@@ -5,6 +5,26 @@ import Constants from 'expo-constants';
 import Feed from './screens/Feed';
 
 export default class App extends React.Component {
+  state = {
+    commentsForItem: {},
+    showModal: false,
+    selectedItemId: null,
+  };
+
+  openCommentScreen = id => {
+    this.setState({
+      showModal: true,
+      selectedItemId: id,
+    });
+  };
+
+  closeCommentScreen = () => {
+    this.setState({
+      showModal: false,
+      selectedItemId: null,
+    });
+  };
+
   render() {
     return(
       <View style={styles.container}>
